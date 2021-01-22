4554c9897a4bbca8a06ba6fb4618e12d0298b7cd
(ari shinoda) <(toreha.ro.se.1148@gmail.com)>
2021-01-23 07:36:30 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@
    import "./index.scss";
    import "./iso.js";
    import "./three-index.js";
+   import anime from 'animejs';
+   // Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+   import * as firebase from "firebase/app";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCJ1dl5kodZeahOQErLLKZqJVMqprVYvkE",
+    authDomain: "azuredesign-id.firebaseapp.com",
+    projectId: "azuredesign-id",
+    storageBucket: "azuredesign-id.appspot.com",
+    messagingSenderId: "620153448404",
+    appId: "1:620153448404:web:90e1b333839f7189025841"
+  };
+
+  // Add the Firebase services that you want to use
 
    window.addEventListener('load', init);
 
@@ -53,6 +68,22 @@
         })
       })
     })
+
+    // const spnavOpen = document.getElementById('open');
+
+    // spnavOpen.addEventListener('click' , function() {
+    //   anime({
+    //     target: '.spnav-wrapper_linebox--item',
+    //     translateX: 80,
+    //     delay: anime.stagger(100) // increase delay by 100ms for each elements.
+    //   }),
+    //   anime({
+    //     target: '.spnav-content',
+    //     translateX: -270,
+    //     easing: 'easeOutExpo',
+    //     duration: 1000
+    //   })
+    // })
     
    }
 
